78008b8bd2ed39590908808306451ad9c91d9e2d
sumanta <sumantajashh@gmail.com>
2025-07-23 12:21:00 +0530

    feature : Created a backend folder, and did set up a maven project & added basic packages, and folder structure

---
+++ app(backend)/src/main/java/com/knockoutzone/backend/service/demoClass.java
@@ -0,0 +1,10 @@
+package com.knockoutzone.backend.service;
+
+/* This class is added just as a placeholder
+ * To maintain a proper folder structure
+ * This service folder will contain all the interfaces
+ * Implementation of those interfaces will be stored in the "impl" subfolder
+ * After creating any interface under service folder, this class will be safe to delete
+*/
+public class demoClass {
+}
---
+++ app(backend)/src/test/java/com/knockoutzone/backend/KnockOutZoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knockoutzone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockOutZoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
