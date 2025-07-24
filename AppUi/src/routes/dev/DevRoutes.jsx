058f07031eb48ca33e5b88177597ccfcd7d48a0a
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-07-24 23:19:08 +0530

    Update DevRoutes.jsx

--- AppUi/src/routes/dev/DevRoutes.jsx
+++ AppUi/src/routes/dev/DevRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import ButtonHelp from "../../pages/help/ButtonHelp";
+import ToastDemo from "../../pages/help/ToastDemo";
 
 const DevRoutes = () => {
   return (
     <>
       <Route path="/dev/help/button" element={<ButtonHelp />} />
+      <Route path="/dev/help/toast" element={<ToastDemo />} />
     </>
   );
 };
